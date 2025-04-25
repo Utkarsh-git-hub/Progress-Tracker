fbb8341c71b8a6cb65584d97e386518bc9727320
Utkarsh Singh <utkarsh862003@gmail.com>
2025-04-26 01:46:41 +0530

    added deployment url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.DEV
+  ? 'http://localhost:5000/api'
+  : `${import.meta.env.VITE_BACKEND_URL}/api`;
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = token;
+  if (token) {
+    config.headers.Authorization = token;
+  }
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
+
